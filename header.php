cac3699df2ed6951a0a688c4897220794b4f15ff
sinhapaurush <76876045+sinhapaurush@users.noreply.github.com>
2021-02-11 11:52:05 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<style>
+    #tminc-tabview{
+        width:100%;
+        position:fixed;
+        height:max-content;
+    }
+    #tminc-tabview li{
+        cursor:pointer;
+        margin:10px;
+        font-size:30px;
+    }
+</style>
